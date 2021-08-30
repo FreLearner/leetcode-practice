b0bc849a8918aaa154355cd6e3d23b4de6e1db20
liaoxh_sch <774051169@qq.com>
2021-08-30 10:15:15 +0800

    5

---
+++ pickIndex-0007-528/src/test/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] a = {};
+        Solution s = new Solution(a);
+        System.out.println(s.pickIndex());
+        System.out.println(s.pickIndex());
+        System.out.println(s.pickIndex());
+        System.out.println(s.pickIndex());
+        System.out.println(s.pickIndex());
+    }
+}
