41a83b53cae6ae2704d452b9b05032a3846abdf2
liaoxh_sch <774051169@qq.com>
2021-09-06 09:21:10 +0800

    11

---
+++ 0016-search-704/src/test/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {-1,0,3,5,9,12};
+        int target = 2;
+        System.out.println(s.search(nums,target));
+    }
+}
