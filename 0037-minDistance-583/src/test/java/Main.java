9f77f062ac80f3af94deb39b1026a04154a6d317
liaoxh_sch <774051169@qq.com>
2021-09-25 10:59:23 +0800

    not bad

---
+++ 0037-minDistance-583/src/test/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String word1 = "sea";
+        String word2 = "eat";
+        System.out.println(s.minDistance(word1,word2));
+    }
+}
