70dc3d59b7415c684e539bf5a54952bd16e39ae6
liaoxh_sch <774051169@qq.com>
2021-09-29 10:24:52 +0800

    not bad

---
+++ 0041-findMinMoves-517/src/test/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] machines = {4,0,0,4};
+        System.out.println(s.findMinMoves(machines));
+    }
+}
