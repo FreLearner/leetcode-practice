6f99ebee39d761c0bdad5a403c5cfdbb56fc105c
FreLearner <774051169@qq.com>
2021-10-04 09:58:02 +0800

    ok

---
+++ 0046-licenseKeyFormatting-482/src/main/java/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuffer sb = new StringBuffer();
+        int count=0;
+        for (int i = s.length()-1; i >-1 ; i--) {
+            if(count==k){
+                count=0;
+                sb.insert(0,'-');
+            }
+            if(s.charAt(i)!='-'){
+                count++;
+                sb.insert(0,s.charAt(i));
+            }
+        }
+        if(sb.length()>0&&sb.charAt(0)=='-'){
+            return sb.substring(1).toString().toUpperCase();
+        }
+        return sb.toString().toUpperCase();
+    }
+}
