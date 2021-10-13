fef6c9de6f8fa58ffbf4793dc3e3fff97dd33ab8
liaoxh_sch <774051169@qq.com>
2021-10-13 09:45:03 +0800

    ok

---
+++ 0055-fizzBuzz-412/src/test/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        for (String str :
+            s.fizzBuzz(15)) {
+            System.out.println(str);
+        }
+
+    }
+}
