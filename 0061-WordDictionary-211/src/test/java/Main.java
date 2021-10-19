1990cee450665abc57571079827f900a15bb7650
liaoxh_sch <774051169@qq.com>
2021-10-19 10:55:03 +0800

    not bad

---
+++ 0061-WordDictionary-211/src/test/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        WordDictionary wordDictionary = new WordDictionary();
+        wordDictionary.addWord("bad");
+        wordDictionary.addWord("dad");
+        wordDictionary.addWord("mad");
+        wordDictionary.search("pad"); // return False
+        wordDictionary.search("bad"); // return True
+        wordDictionary.search(".ad"); // return True
+        wordDictionary.search("b.."); // return True
+
+    }
+}
