80759e46f043384fd9083c3027bd745b5594ed95
liaoxh_sch <774051169@qq.com>
2021-10-26 14:33:32 +0800

    not bad

---
+++ 0068-nextGreaterElement-496/src/test/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] input1 = {4,1,2};
+        int[] input2 = {1,3,4,2};
+        System.out.println(s.nextGreaterElement(input1,input2));
+    }
+}
